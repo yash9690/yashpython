c48e3bb3a1410acafacbe09dc8cac4e11871e5c2
yash9690 <146159267+yash9690@users.noreply.github.com>
2023-09-29 11:44:28 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+#print(100)
+print("hello world!")
+print("hey there")
+x=100
+a=3.6
+z="hello yash"
+print(x,z,a)
+#printing data type of variables
+print(type(z))
+#math operation
+sum1=x+a
+mul1=x*a
+print(sum1,mul1)
---
+++ task1.py
@@ -0,0 +1,6 @@
+#by default input takes all arguement as string
+
+a=(int)(input("enter the first number"))
+b=(int)(input("enter second number"))
+sum=a*b
+print (sum)
